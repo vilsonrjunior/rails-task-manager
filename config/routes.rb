9bb9c00d3ba53c5bcffe0e434b3564a728bc2e5e
vilson junior <vilsonrjunior@gmail.com>
2019-02-08 18:56:54 +0800

    ' task manager init '

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: :tasks
   get 'tasks/new', to: 'tasks#new', as: :new
-  get 'tasks/:id', to: 'tasks#show', as: "task"
+  get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'restaurants#update'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
 
 
